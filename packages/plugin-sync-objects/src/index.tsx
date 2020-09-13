843a97fc8f9aa88f5a241df45bdca6b4b29db8b1
Gabriel Rodrigues <jipacoding@gmail.com>
2020-09-13 19:17:04 -0400

    feat(plugin-sync-objects): watchToChanges
    Related to #37

--- packages/plugin-sync-objects/src/index.tsx
+++ packages/plugin-sync-objects/src/index.tsx
@@ -18,7 +18,23 @@
  */
 //endregion
 
+import { fabric } from "fabric";
+import { findIndexByObject } from "@ui5/shared-lib/lib/canvas/find-index-by-object";
 import { EditorPlugin } from "@ui5/shared-lib/lib/editor/EditorPlugin";
+import { Canvas, Object } from "fabric/fabric-impl";
+import * as lib from "@fantastic-images/lib";
+
+const {
+  model: {
+    mutations: { UPDATE_OBJECT },
+  },
+} = lib;
+
+const reactive = (canvas: Canvas) => (trigger: string, fn: any) =>
+  trigger.split(" ").forEach((i) => canvas.on(i, fn));
+
+const getTargets = (e: any): Object[] =>
+  e.target._objects ? e.target._objects : e.target.group._objects;
 
 export default class SyncObjects extends EditorPlugin {
   onRegisterPlugin() {
@@ -30,4 +46,48 @@ export default class SyncObjects extends EditorPlugin {
   }
   onSetup() {}
   async onMount() {}
+  async watchToChanges() {
+    if (this.canvas && this.editor) {
+      const { canvas } = this;
+      const _reactive = reactive(canvas);
+      const findIndexByObject_ = findIndexByObject(canvas);
+      _reactive("object:modified", async (e: any) => {
+        if (!this.editor) return;
+        const { template } = this.editor.state;
+        const target = getTargets(e);
+        await this.editor.onSetTemplate(
+          target
+            .map(
+              (obj) =>
+                [
+                  obj,
+                  findIndexByObject_(obj) -
+                    template.model.staticImages.filter(
+                      ({ position }) => position === "back",
+                    ).length,
+                  findIndexByObject_(obj),
+                ] as [Object, number, number],
+            )
+            .map(([object, itemIndex, canvasIndex]): [any, number, number] => {
+              const { x: left, y: top } = fabric.util.transformPoint(
+                new fabric.Point(object.left || 0, object.top || 0),
+                (object.group as any).calcOwnMatrix(),
+              );
+              return [
+                { ...object.toObject(), left, top },
+                itemIndex,
+                canvasIndex,
+              ];
+            })
+            .reduce(
+              (acc, [object, idx]) => UPDATE_OBJECT(idx, object)(acc),
+              template,
+            ),
+        );
+      });
+    }
+  }
+  async onSetupCanvas() {
+    await this.watchToChanges();
+  }
 }

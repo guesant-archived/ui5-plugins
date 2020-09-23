4b2d6de6209913100de6ed98a3e84becec42b2c7
Gabriel Rodrigues <jipacoding@gmail.com>
2020-09-22 20:57:15 -0400

    feat(inspect-object-alignment): modes/align

---
+++ packages/inspect-object-alignment/src/modes/align/index.tsx
@@ -0,0 +1,43 @@
+//region Preamble
+/**
+ * https://github.com/guesant/ui5-monorepo
+ * Copyright (C) 2020 Gabriel Rodrigues
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+//endregion
+
+import * as React from "react";
+import InspectObjectAlignment from "../..";
+import { Grid, GridButton } from "../../components/grid";
+
+export const align = (plugin: InspectObjectAlignment) => () => {
+  if (!plugin.editor) return <div />;
+  const modifiers: React.ButtonHTMLAttributes<HTMLButtonElement>[] = [];
+  return (
+    <Grid
+      children={
+        <React.Fragment>
+          {modifiers.map((modf, idx) =>
+            React.createElement(
+              React.Fragment,
+              { key: idx },
+              React.createElement(GridButton, { ...modf }),
+            ),
+          )}
+        </React.Fragment>
+      }
+    />
+  );
+};

eafb69436fdee12df4fac9ab6a6d30dffc6a5286
Gabriel Rodrigues <jipacoding@gmail.com>
2020-09-22 20:52:22 -0400

    feat(inspect-object-alignment): generateVerticalCenter

---
+++ packages/inspect-object-alignment/src/helpers/generate-vertical-center.ts
@@ -0,0 +1,37 @@
+//region Preamble
+/**
+ * https://github.com/guesant/ui5-monorepo
+ * Copyright (C) 2020 Gabriel Rodrigues
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+//endregion
+
+import { getBoundingRect } from "@ui5/shared-lib/lib/template/get-bounding-rect";
+import { getGroupBox } from "@ui5/shared-lib/lib/template/get-group-box";
+import { fnFunction } from "@ui5/shared-lib/lib/template/update-selected-items";
+import { AsGroup } from "./as-group-from-event";
+import { generateVerticalTop } from "./generate-vertical-top";
+
+export const generateVerticalCenter = (
+  medium: number,
+  asGroup: AsGroup = false,
+): fnFunction => (fnArgs) =>
+  generateVerticalTop(
+    medium -
+      (asGroup
+        ? getGroupBox(fnArgs.objects).height / 2
+        : getBoundingRect(fnArgs.object).boxHeight / 2),
+    asGroup,
+  )(fnArgs);
